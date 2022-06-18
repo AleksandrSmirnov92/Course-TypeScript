9a150b3578f5eb399df17ded243f2ffb3a504a0a
AleksandrSmirnov92 <ryan00@mail.ru>
2022-06-18 21:39:16 +0300

    Типы в функциях

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ let a = 1;
 let b = 5;
 let j = a + b;
 console.log(j);
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`;
+}
+console.log(getFullName("hai", "hello"));
--- app.ts
+++ app.ts
@@ -2,3 +2,7 @@ let a = 1
 let b:number = 5
 let j:number = a + b
 console.log(j)
+function getFullName (firstName:string, lastName:string) {
+    return `${firstName} ${lastName}`
+}
+console.log(getFullName("hai","hello"))
